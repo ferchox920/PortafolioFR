25445b5a5707d5d9a4d9552479bdc096b1a66fbd
ferchox920 <fernandoramones92@gmail.com>
2023-02-27 18:10:56 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import Navbar from './Components/NavBar/NavBar';
+import Navbar from "./Components/NavBar/NavBar";
 import { Route, Routes } from "react-router";
-import './App.css';
-import Home from './View/Home/Home';
-import LandingPage from './View/Landing/LandingPage';
+import "./App.css";
+import Home from "./View/Home/Home";
+import LandingPage from "./View/Landing/LandingPage";
+
+import Cards from "./Components/Cards/Cards";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/landing" element={<LandingPage/>} />
-      </Routes> 
+      <Navbar />
+      <Cards />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
