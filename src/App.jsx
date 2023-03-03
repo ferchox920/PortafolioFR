1eca82d20378b2051dc156349f1849af8bc4cf1f
Andrea Ramones <fernandoramones92@gmail.com>
2023-03-03 14:56:46 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Components/NavBar/NavBar";
-import Cards from "./Components/Cards/Cards";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
 import Home from "./View/Home/Home";
