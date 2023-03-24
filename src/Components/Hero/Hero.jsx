3f259f5ad6e496dc9f4231fad202ebf03022ec55
ferchox920 <fernandoramones92@gmail.com>
2023-03-24 15:22:46 -0300

    commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -60,6 +60,14 @@ const Hero = () => {
               <Carousel.Item>
                 <img
                   className="d-block w-100 carousel-img"
+                  src={ia}
+                  alt="Zero slide"
+                  style={{ height: "400px", objectFit: "cover" }}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 carousel-img"
                   src={io}
                   alt="First slide"
                   style={{ height: "400px", objectFit: "cover" }}
